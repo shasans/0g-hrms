9a7e04195a15112833a8d9c6b1a3bbd917817216
shasans <55390937+shasans@users.noreply.github.com>
2020-09-09 00:31:29 +0400

    Add files via upload

---
+++ Zero/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zero.Pages
+{
+    [AllowAnonymous]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
