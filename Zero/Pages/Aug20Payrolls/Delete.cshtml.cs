b215788f43d6f26e3d82faf5e051be06a01f9f18
shasans <55390937+shasans@users.noreply.github.com>
2020-09-09 00:34:27 +0400

    Add files via upload

---
+++ Zero/Pages/Aug20Payrolls/Delete.cshtml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+namespace Zero.Pages.Aug20Payrolls
+{
+    public class DeleteModel : DI_BasePageModel
+    {
+        //  private readonly Zero.Data.ApplicationDbContext _context;
+
+        public DeleteModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        [BindProperty]
+        public Aug20Payroll Aug20Payroll { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+
+            Aug20Payroll = await Context.Aug20Payroll
+                .Include(j => j.Employee).FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (Aug20Payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, Aug20Payroll,
+                                                     EmployeeOperations.Delete);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            return Page();
+
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            Aug20Payroll = await Context.Aug20Payroll.FindAsync(id);
+
+            var aug20payroll = await Context
+                .Apr20Payroll.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (aug20payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, aug20payroll,
+                                                     EmployeeOperations.Delete);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            Context.Aug20Payroll.Remove(Aug20Payroll);
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+
+
+        }
+    }
+}
