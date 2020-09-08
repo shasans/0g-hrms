3a951c32f767953aa758e5be3c1b490d0fb01006
shasans <55390937+shasans@users.noreply.github.com>
2020-09-09 00:32:07 +0400

    Add files via upload

---
+++ Zero/Pages/Apr19Payrolls/Delete.cshtml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+namespace Zero.Pages.Apr19Payrolls
+{
+    public class DeleteModel : DI_BasePageModel
+    {
+        //  private readonly Zero.Data.ApplicationDbContext _context;
+
+        public DeleteModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        [BindProperty]
+        public Apr19Payroll Apr19Payroll { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+
+            Apr19Payroll = await Context.Apr19Payroll
+                .Include(j => j.Employee).FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (Apr19Payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, Apr19Payroll,
+                                                     EmployeeOperations.Delete);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            return Page();
+
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            Apr19Payroll = await Context.Apr19Payroll.FindAsync(id);
+
+            var apr19payroll = await Context
+                .Apr19Payroll.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (apr19payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, apr19payroll,
+                                                     EmployeeOperations.Delete);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            Context.Apr19Payroll.Remove(Apr19Payroll);
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+
+
+        }
+    }
+}
---
+++ Zero/Pages/Apr19Payrolls/Edit.cshtml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+namespace Zero.Pages.Apr19Payrolls
+{
+    public class EditModel : DI_BasePageModel
+    {
+        //  private readonly Zero.Data.ApplicationDbContext _context;
+
+        public EditModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        [BindProperty]
+        public Apr19Payroll Apr19Payroll { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Apr19Payroll = await Context.Apr19Payroll.Include(j => j.Employee).FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (Apr19Payroll == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeID"] = new SelectList(Context.Employee, "ID", "FullName");
+            // return Page();
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                  User, Apr19Payroll,
+                                                  EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ///
+            // Fetch Contact from DB to get OwnerID.
+            var apr19payroll = await Context
+                .Apr19Payroll.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (apr19payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, apr19payroll,
+                                                     EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            Apr19Payroll.OwnerID = apr19payroll.OwnerID;
+
+            Context.Attach(Apr19Payroll).State = EntityState.Modified;
+
+
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool Apr19PayrollExists(int id)
+        {
+            return Context.Apr19Payroll.Any(e => e.EmployeeID == id);
+        }
+    }
+}
+
+
