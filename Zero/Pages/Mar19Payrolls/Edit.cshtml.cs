d5879a07694da17578a67e1acb16d397a263ec0f
shasans <55390937+shasans@users.noreply.github.com>
2020-09-09 21:56:48 +0400

    Add files via upload

---
+++ Zero/Pages/Mar19Payrolls/Edit.cshtml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+namespace Zero.Pages.Mar19Payrolls
+{
+    public class EditModel : DI_BasePageModel
+    {
+        //  private readonly Zero.Data.ApplicationDbContext _context;
+
+        public EditModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        [BindProperty]
+        public Mar19Payroll Mar19Payroll { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Mar19Payroll = await Context.Mar19Payroll.Include(j => j.Employee).FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (Mar19Payroll == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeID"] = new SelectList(Context.Employee, "ID", "FullName");
+            // return Page();
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                  User, Mar19Payroll,
+                                                  EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ///
+            // Fetch Contact from DB to get OwnerID.
+            var mar19payroll = await Context
+                .Mar19Payroll.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (mar19payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, mar19payroll,
+                                                     EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            Mar19Payroll.OwnerID = mar19payroll.OwnerID;
+
+            Context.Attach(Mar19Payroll).State = EntityState.Modified;
+
+
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool Mar19PayrollExists(int id)
+        {
+            return Context.Mar19Payroll.Any(e => e.EmployeeID == id);
+        }
+    }
+}
+
+
---
+++ Zero/Pages/Mar19Payrolls/Index.cshtml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.WebSockets.Internal;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+
+namespace Zero.Pages.Mar19Payrolls
+{
+    public class IndexModel : DI_BasePageModel
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        public IndexModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager, IHostingEnvironment hostingEnvironment)
+        : base(context, authorizationService, userManager)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+        public IList<Mar19Payroll> Mar19Payroll { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var mar19payrolls = from c in Context.Mar19Payroll.Include(j => j.Employee)
+              .AsNoTracking()
+                                select c;
+
+            Mar19Payroll = await mar19payrolls.ToListAsync();
+
+            foreach (var item in Mar19Payroll)
+            {
+                item.Total = CalculateTotal(item.Employee.Salary, item.Employee.Allowance1, item.Employee.Allowance2, item.Employee.Allowance3, item.Deduction, CalcOvertime((item.Employee.Salary), (item.OvertimeHours)), item.Bonus);
+            }
+            var data = Mar19Payroll.Sum(i => i.Total);
+            HttpContext.Session.SetInt32("mar19total", data);//set session here
+        }
+        int CalcOvertime(int salary, int hours)
+        {
+            return ((salary / ((31 - 5) * 8)) * hours);
+        }
+        int CalculateTotal(int salary, int a1, int a2, int a3, int deduction, int overtime, int bonus)
+        {
+            return (salary + a1 + a2 + a3 - deduction + overtime + bonus);
+        }
+
+        public async Task<IActionResult> OnPostExporttoExcel()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string fileName = @"Mar19Payroll.xlsx";
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, fileName);
+            FileInfo file = new FileInfo(Path.Combine(webRootPath, fileName));
+            var memoryStream = new MemoryStream();
+            // --- Below code would create excel file with dummy data----  
+            using (var fs = new FileStream(Path.Combine(webRootPath, fileName), FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Mar19Payroll");
+
+                IRow row = excelSheet.CreateRow(0);
+                row.CreateCell(0).SetCellValue("EmployeeID");
+                row.CreateCell(1).SetCellValue("FullName");
+                row.CreateCell(2).SetCellValue("Salary");
+                row.CreateCell(3).SetCellValue("Allowance1");
+                row.CreateCell(4).SetCellValue("Allowance2");
+                row.CreateCell(5).SetCellValue("Allowance3");
+                row.CreateCell(6).SetCellValue("Deduction");
+                row.CreateCell(7).SetCellValue("OvertimeHours");
+                row.CreateCell(8).SetCellValue("Bonus");
+                row.CreateCell(9).SetCellValue("Total");
+
+
+                var mar19payrolls = from s in Context.Mar19Payroll.Include(j => j.Employee)
+                .AsNoTracking()
+                                    select s;
+                int counter = 1;
+                foreach (var mar19payroll in mar19payrolls)
+                {
+                    row = excelSheet.CreateRow(counter);
+                    row.CreateCell(0).SetCellValue(mar19payroll.Employee.ID);
+                    row.CreateCell(1).SetCellValue(mar19payroll.Employee.FullName);
+                    row.CreateCell(2).SetCellValue(mar19payroll.Employee.Salary);
+                    row.CreateCell(3).SetCellValue(mar19payroll.Employee.Allowance1);
+                    row.CreateCell(4).SetCellValue(mar19payroll.Employee.Allowance2);
+                    row.CreateCell(5).SetCellValue(mar19payroll.Employee.Allowance3);
+                    row.CreateCell(6).SetCellValue(mar19payroll.Deduction);
+                    row.CreateCell(7).SetCellValue(mar19payroll.OvertimeHours);
+                    row.CreateCell(8).SetCellValue(mar19payroll.Bonus);
+                    row.CreateCell(9).SetCellValue(CalculateTotal(mar19payroll.Employee.Salary, mar19payroll.Employee.Allowance1, mar19payroll.Employee.Allowance2, mar19payroll.Employee.Allowance3, mar19payroll.Deduction, CalcOvertime((mar19payroll.Employee.Salary), (mar19payroll.OvertimeHours)), mar19payroll.Bonus));
+                    counter++;
+                }
+                workbook.Write(fs);
+            }
+            using (var fileStream = new FileStream(Path.Combine(webRootPath, fileName), FileMode.Open))
+            {
+                await fileStream.CopyToAsync(memoryStream);
+            }
+            memoryStream.Position = 0;
+            return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        }
+
+    }
+    
+}
