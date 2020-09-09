22f63fefaec15d060da6c0d343caea5e48171260
shasans <55390937+shasans@users.noreply.github.com>
2020-09-09 21:53:19 +0400

    Add files via upload

---
+++ Zero/Pages/Jul19Payrolls/Edit.cshtml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Zero.Authorization;
+using Zero.Data;
+using Zero.Models;
+using Zero.Pages.Employees;
+
+namespace Zero.Pages.Jul19Payrolls
+{
+    public class EditModel : DI_BasePageModel
+    {
+        //  private readonly Zero.Data.ApplicationDbContext _context;
+
+        public EditModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        [BindProperty]
+        public Jul19Payroll Jul19Payroll { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Jul19Payroll = await Context.Jul19Payroll.Include(j => j.Employee).FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (Jul19Payroll == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeID"] = new SelectList(Context.Employee, "ID", "FullName");
+            // return Page();
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                  User, Jul19Payroll,
+                                                  EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ///
+            // Fetch Contact from DB to get OwnerID.
+            var jul19payroll = await Context
+                .Jul19Payroll.AsNoTracking()
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+
+            if (jul19payroll == null)
+            {
+                return NotFound();
+            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                     User, jul19payroll,
+                                                     EmployeeOperations.Update);
+            if (!isAuthorized.Succeeded)
+            {
+                return new ChallengeResult();
+            }
+
+            Jul19Payroll.OwnerID = jul19payroll.OwnerID;
+
+            Context.Attach(Jul19Payroll).State = EntityState.Modified;
+
+
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool Jul19PayrollExists(int id)
+        {
+            return Context.Jul19Payroll.Any(e => e.EmployeeID == id);
+        }
+    }
+}
+
+
